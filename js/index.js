fdd5f13422df24ca60c3ebf79214a9fe05076518
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2024-08-12 00:58:23 -0500

    end

--- js/index.js
+++ js/index.js
@@ -22,5 +22,68 @@ pdf_buton.onclick=function(){
         
        
         var doc = new jsPDF('p', 'mm', 'a3');
-        doc.addImage(img, 'JPEG', 50, 50, 200, 300);
+        doc.addImage(img, 'JPEG', 30, 30, 220, 350);
     doc.save('Infograma_AHSolutions_.pdf')}
+
+
+
+
+    /*var myState = {
+        pdf: null,
+        currentPage: 1,
+        zoom: 1
+    }
+    pdfjsLib.getDocument('./img/AUTOMATIC HOME SOLUTIONS- PORTAFOLIO.pdf').then((pdf) => {
+        myState.pdf = pdf;
+        render();
+    });
+    
+
+    function render() {
+        myState.pdf.getPage(myState.currentPage).then((page) => {
+            var canvas = document.getElementById("pdf_renderer");
+            var ctx = canvas.getContext('2d');
+            var viewport = page.getViewport(myState.zoom);
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+            page.render({
+                canvasContext: ctx,
+                viewport: viewport
+            });
+        });
+    }
+    document.getElementById('go_previous')
+        .addEventListener('click', (e) => {
+            if(myState.pdf == null
+               || myState.currentPage == 1) return;
+            myState.currentPage -= 1;
+            document.getElementById("current_page")
+                    .value = myState.currentPage;
+            render();
+        });
+        document.getElementById('go_next')
+        .addEventListener('click', (e) => {
+            if(myState.pdf == null 
+               || myState.currentPage > myState.pdf
+                                               ._pdfInfo.numPages) 
+               return;
+        
+            myState.currentPage += 1;
+            document.getElementById("current_page")
+                    .value = myState.currentPage;
+            render();
+        });
+        document.getElementById('zoom_in')
+        .addEventListener('click', (e) => {
+            if(myState.pdf == null) return;
+            myState.zoom += 0.5;
+            render();
+        });
+        document.getElementById('zoom_out')
+        .addEventListener('click', (e) => {
+            if(myState.pdf == null) return;
+            myState.zoom -= 0.5;
+            render();
+        });
+
+    */
